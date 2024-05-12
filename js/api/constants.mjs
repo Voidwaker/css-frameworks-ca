42643b302cdf971c4333050fc0cdd0bef788c068
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-05-12 22:18:30 +0200

    api
     export base api

---
+++ js/api/constants.mjs
@@ -0,0 +1,4 @@
+export const API_HOST_URL = 'https://api.noroff.dev/api/v1';
+export const API_BASE = '/api/v1';
+export const API_SOCIAL_BASE = '/social';
+export const API_SOCIAL_URL = `${API_HOST_URL}${API_SOCIAL_BASE}`;
