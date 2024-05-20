f085ef13f441c529e750d1fc91bbeca6a80aa2b6
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-05-20 22:24:55 +0200

    fetching posts
    fetching posts, and removing some hardcoded "posts" that i used for the css-frameworks assignment

--- js/index.mjs
+++ js/index.mjs
@@ -8,12 +8,36 @@ import * as post from "./api/posts/index.mjs";
         setRegisterFormListener();
     } else if (path === "/") {
         setloginFormListener();
-    } 
+    }
+    
+    if (path === "/feed/index.html") {
+        post.getPosts()
+            .then(posts => {
+                console.log(posts);
+    
+            
+                const postsContainer = document.getElementById('postsContainer');
+    
+                
+                posts.forEach(post => {
+                    const postElement = document.createElement('div');
+                    postElement.className = 'card mb-3';
+    
+                    postElement.innerHTML = `
+                        <div class="card-body text-green">
+                            <h5 class="card-title">${post.title}</h5>
+                            <p class="card-text">${post.body}</p>
+                        </div>
+                    `;
+    
+                    postsContainer.appendChild(postElement);
+                });
 
+            })
+            .catch(console.error);
+    }
     //post.createPost()
     //post.updatePost()
     //post.removePost()
     //post.getPost()
- 
-    post.getPosts().then(console.log);
 
