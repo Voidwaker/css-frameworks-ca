017ad995e6196792b3f2a26a1cbb3a5942eebb33
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2025-03-22 17:49:18 +0100

    Update constants.mjs

--- js/api/constants.mjs
+++ js/api/constants.mjs
@@ -6,9 +6,7 @@ export const API_REGISTER = `${API_BASE}${API_AUTH}/register`;
 export const API_LOGIN = `${API_BASE}${API_AUTH}/login`;
 export const API_CREATE_API_KEY = `${API_BASE}${API_AUTH}/create-api-key`;
 export const API_SOCIAL_URL = `${API_BASE}/social`;
-
 export const API_KEY_STORAGE = "apiKey"; 
 export const API_TOKEN_STORAGE = "token"; 
 export const API_PROFILE_STORAGE = "profile"; 
-
 export const DEFAULT_API_KEY = "102ac323-5f93-4aa8-b7bd-b2610ddb91bb"; 
